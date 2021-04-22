4e972b24f2437ef9051a50dc409eb48e18b9df88
dhviet12 <73893345+dhviet12@users.noreply.github.com>
2021-04-22 21:29:43 +0700

    tao model Role va User

--- src/app/user/model/signUp.ts
+++ src/app/user/model/signUp.ts
@@ -4,14 +4,16 @@ export class SignUp {
   fullName: string;
   address: string;
   email: string;
+  phone: string;
   role: string[];
 
-  constructor(username: string, password: string, fullName: string, address: string, email: string) {
+  constructor(username: string, password: string, fullName: string, address: string, email: string, phone: string) {
     this.username = username;
     this.password = password;
     this.fullName = fullName;
     this.address = address;
     this.email = email;
+    this.phone = phone;
     this.role = ['ROLE_USER'];
   }
 
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {SignUp} from '../model/signUp';
+import {FormControl, Validators} from '@angular/forms';
+import {FormSignUpService} from '../service/form-sign-up.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  form: any= {};
+  // @ts-ignore
+  signUp: SignUp;
+  status= 'Please fill in the form';
 
-  constructor() { }
+  usernameValidate = new FormControl('',
+    Validators.required)
+
+  passwordValidate = new FormControl('',
+    Validators.required)
+  hide = true;
+  mess1: any ={
+    message: "Fail -> Username is already taken!"
+  }
+  mess2: any ={
+    message: "User registered successfully!"
+  }
+
+
+
+
+
+  constructor(private formSignUpService:FormSignUpService) { }
 
   ngOnInit(): void {
+  }
+  onSubmit(){
+    this.signUp= new SignUp(
+      this.form.username,
+      this.form.password,
+      this.form.fullName,
+      this.form.address,
+      this.form.email,
+      this.form.phone
+    )
+    this.formSignUpService.signUp(this.signUp).subscribe(data=>{
+      console.log('data',data)
+      if(JSON.stringify(data)== JSON.stringify(this.mess1)){
+        this.status= 'Username is existed'
+      }
+      if(JSON.stringify(data)== JSON.stringify(this.mess2)){
+        this.status= 'Created Successfully'
+        alert(this.status)
+      }
+    })
   }
 
 }
