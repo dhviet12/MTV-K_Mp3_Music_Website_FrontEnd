4b8d594699323084fabc01d2fae64d2fc8cfee48
khaitrinh1207 <khaitrinh1207@gmail.com>
2021-04-24 20:14:08 +0700

    sua trang edit song

--- src/app/song/edit-song/edit-song.component.ts
+++ src/app/song/edit-song/edit-song.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {ISong} from '../isong';
-import {Subscription} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
 import {SongService} from '../song.service';
 import {ActivatedRoute, ParamMap, Router} from '@angular/router';
 import {finalize} from 'rxjs/operators';
 import {AngularFireStorage} from '@angular/fire/storage';
+import bsCustomFileInput from 'bs-custom-file-input';
 
 @Component({
   selector: 'app-edit-song',
@@ -12,51 +13,77 @@ import {AngularFireStorage} from '@angular/fire/storage';
   styleUrls: ['./edit-song.component.scss']
 })
 export class EditSongComponent implements OnInit {
-  song: ISong = {};
-  sub: Subscription;
-  upFileMp3(e: any): any {
-    const n = Date.now();
-    const file = e.target.files[0];
-    const filePath = `mp3/${n}`;
-    const fileRef = this.storage.ref(filePath);
-    this.storage.upload(filePath, file).snapshotChanges().pipe(finalize(() => {
-        fileRef.getDownloadURL().subscribe(url => {
-          this.song.fileMp3 = url;
-          console.log(this.song.fileMp3);
-        });
-      })
-    )
-      .subscribe();
-  }
-  upFileImage(event: any): any {
-    const n = Date.now();
-    const file = event.target.files[0];
-    const filePath = `image/${n}`;
-    const fileRef = this.storage.ref(filePath);
-    this.storage.upload(filePath, file).snapshotChanges().pipe(finalize(() => {
-        fileRef.getDownloadURL().subscribe( url => {
-          this.song.fileImage = url;
-          console.log(this.song.fileImage);
-        });
-      })
-    )
-      .subscribe();
-  }
-  // tslint:disable-next-line:max-line-length
-  constructor(private songService: SongService, private activatedRoute: ActivatedRoute, private router: Router, private storage: AngularFireStorage) {
+  constructor(private songService: SongService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router, private storage: AngularFireStorage) {
     this.sub = this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
       this.song.id = Number(paramMap.get('id'));
       this.getSongById(this.song.id);
     });
   }
-  getSongById(id: number){
+  song: ISong = {};
+  sub: Subscription;
+  percentageMp3 = 0;
+  percentageImg = 0;
+  ngOnInit(): void {
+    // hiển thị tên file trên thanh input file
+    bsCustomFileInput.init();
+  }
+  upFileMp3(e: any): Observable<number | undefined> {
+    const file = e.target.files[0];
+    const fileName = file.name;
+    const filePath = `mp3/${fileName}`;
+    const storageRef = this.storage.ref(filePath);
+    const task = this.storage.upload(filePath, file);
+    task.snapshotChanges().pipe(
+      finalize(() => {
+        storageRef.getDownloadURL().subscribe(URL => {
+          this.song.fileMp3 = URL;
+          console.log(this.song.fileMp3);
+        });
+      })
+    ).subscribe();
+    return task.percentageChanges();
+  }
+  // up file ảnh lên firebase storage
+  upFileImage(e: any): Observable<number | undefined> {
+    const file = e.target.files[0];
+    const fileName = file.name;
+    const filePath = `image/${fileName}`;
+    const storageRef = this.storage.ref(filePath);
+    const task = this.storage.upload(filePath, file);
+    task.snapshotChanges().pipe(
+      finalize(() => {
+        storageRef.getDownloadURL().subscribe(URL => {
+          this.song.fileImage = URL;
+          console.log(this.song.fileImage);
+        });
+      })
+    ).subscribe();
+    return task.percentageChanges();
+  }
+  // tiến trình upload fileMp3
+  processUploadMp3(e: any): void{
+    this.upFileMp3(e).subscribe(
+      percentage => {
+        this.percentageMp3 = Math.round(percentage ? percentage : 0);
+      }
+    );
+  }
+  // tiến trình upload file Img
+  processUploadImg(e: any): void{
+    this.upFileImage(e).subscribe(
+      percentage => {
+        this.percentageImg = Math.round(percentage ? percentage : 0);
+      }
+    );
+  }
+  getSongById(id: number): any{
     return this.songService.getSongById(id).subscribe(song => {
       this.song = song;
     });
   }
-  editSong(){
+  editSong(): any{
     return this.songService.editSong(this.song.id, this.song).subscribe(window.location.reload);
-  }
-  ngOnInit(): void {
   }
 }
