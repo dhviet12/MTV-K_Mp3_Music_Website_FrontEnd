87c81beb7ef033aee3bca5269da7ee1048767e3e
Your Name <you@example.com>
2021-04-27 01:16:28 +0700

    add comment

--- src/app/comment/icomment.ts
+++ src/app/comment/icomment.ts
@@ -1,7 +1,9 @@
 import {IUser} from "../user/model/IUser";
+import {ISong} from "../song/isong";
 
 export interface IComment {
-  id: number;
+  id : number;
   content?: string;
-  user?: any;
+  user: IUser;
+  song: ISong;
 }
