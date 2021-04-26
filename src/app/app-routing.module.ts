3e2906935971407425f7f175e52a841e541382d9
khaitrinh1207 <khaitrinh1207@gmail.com>
2021-04-26 13:52:13 +0700

    upfile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {RegisterComponent} from './user/register/register.component';
-import {LoginComponent} from './user/login/login.component';
-import {SignoutComponent} from './user/signout/signout.component';
 
 const routes: Routes = [
   {
     path: 'songs',
     loadChildren: () => import('./song/song.module').then(module => module.SongModule)
   },
-  { path:'signup',
-    component: RegisterComponent
-  },
   {
-    path:'login',
-    component: LoginComponent
-  },
-  {
-    path:'logout',
-    component: SignoutComponent
-  },
-  {
-    path:'user',
+    path: 'user',
     loadChildren: () => import('./user/user.module').then(module => module.UserModule)
   }
 ];
