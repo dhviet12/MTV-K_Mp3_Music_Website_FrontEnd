acc9e1d6a343cacf6908432d055c3891cad82267
khaitrinh1207 <khaitrinh1207@gmail.com>
2021-04-26 15:30:33 +0700

    update form profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {RegisterComponent} from './user/register/register.component';
 
 const routes: Routes = [
   {
