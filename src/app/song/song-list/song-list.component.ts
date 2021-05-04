44ef09114ae3dcfbfc3bbde3a55b0094feeb08e4
khaitrinh1207 <khaitrinh1207@gmail.com>
2021-05-04 08:17:17 +0700

    fix git

--- src/app/song/song-list/song-list.component.ts
+++ src/app/song/song-list/song-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {SongService} from '../song.service';
 import {ISong} from '../isong';
 import {Router} from '@angular/router';
+import {DataService} from '../../shared/ dataTransmission/data.service';
 
 @Component({
   selector: 'app-song-list',
@@ -9,38 +10,34 @@ import {Router} from '@angular/router';
   styleUrls: ['./song-list.component.scss']
 })
 export class SongListComponent implements OnInit {
-  constructor(private songService: SongService, private router: Router) {
+  constructor(private songService: SongService,
+              private data: DataService) {
     this.getAllSong();
   }
-  keyword: any;
+
   songList: ISong[] = [];
+
   getAllSong(): any {
-    return this.songService.getAllSong().subscribe( songs => {
+    return this.songService.getAllSong().subscribe(songs => {
       this.songList = songs;
     });
   }
-  delSong(id: number): any {
-    if (confirm('Bạn có chắc muốn xoá bài hát này ?')){
-      this.songService.delSong(id).subscribe( () => {
-        this.router.routeReuseStrategy.shouldReuseRoute = () => false;
-        this.router.onSameUrlNavigation = 'reload';
-        this.router.navigateByUrl('songs') ;
-      });
-    }
 
+  // playSong(song: any): any {
+  //   console.log(song);
+  //   this.data.changeData(song);
+  // }
+
+  playAlbum(album: any): any {
+    // console.log(album);
+    this.data.changeAlbum(album);
   }
+
   // xóa bái hát trên firebase
   // delSong(): any{
   //   const storageRef = this.storage.ref('mp3/');
   //   storageRef.child('audio_xe_dap_oi.mp3').delete();
   // }
-  searchSong(): any {
-    this.songService.searchSong(this.keyword).subscribe(songList => {
-      console.log(this.keyword);
-      this.songList = songList;
-      console.log(this.songList);
-    });
-  }
   ngOnInit(): void {
   }
 }
