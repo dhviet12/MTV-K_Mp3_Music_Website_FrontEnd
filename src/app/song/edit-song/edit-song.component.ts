665ea23f1cfa3bda16da01ca5bacfa7297b3e0b5
khaitrinh1207 <khaitrinh1207@gmail.com>
2021-05-05 12:03:33 +0700

    update form update

--- src/app/song/edit-song/edit-song.component.ts
+++ src/app/song/edit-song/edit-song.component.ts
@@ -7,6 +7,11 @@ import {finalize} from 'rxjs/operators';
 import {AngularFireStorage} from '@angular/fire/storage';
 import bsCustomFileInput from 'bs-custom-file-input';
 import {FormBuilder, Validators} from '@angular/forms';
+import {Icategory} from '../../model/icategory';
+import {PlayList} from '../../play-list/play-list';
+import {CategoryService} from '../../service/categoryService/category.service';
+import {PlayListService} from '../../play-list/play-list.service';
+import {AuthenService} from '../../user/service/authen.service';
 
 @Component({
   selector: 'app-edit-song',
@@ -18,14 +23,18 @@ export class EditSongComponent implements OnInit {
               private activatedRoute: ActivatedRoute,
               private router: Router,
               private storage: AngularFireStorage,
-              private fb: FormBuilder
+              private fb: FormBuilder,
+              private cateService: CategoryService,
+              private playlistService: PlayListService,
+              private authen: AuthenService
   ) {
     this.sub = this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
       this.song.id = Number(paramMap.get('id'));
       this.getSongById(this.song.id);
     });
   }
-
+  categories: Icategory[] = [];
+  playList: PlayList[] = [];
   song: ISong = {
     id: 0
   };
@@ -41,12 +50,16 @@ export class EditSongComponent implements OnInit {
       Validators.pattern('^[a-zA-Z0-9ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêếìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂẾưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹý ]+$')]],
     author: ['', [Validators.required, Validators.minLength(2),
       Validators.pattern('^[a-zA-Z0-9ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêếìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂẾưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹý ]+$')]],
-    description: ['', Validators.maxLength(300)]
+    description: ['', Validators.maxLength(300)],
+    category: ['', [Validators.required]],
+    playList: ['']
   });
 
   ngOnInit(): void {
     // hiển thị tên file trên thanh input file
     bsCustomFileInput.init();
+    this.getAllCate();
+    this.getAllPlayList();
   }
 
   upFileMp3(e: any): Observable<number | undefined> {
@@ -102,7 +115,7 @@ export class EditSongComponent implements OnInit {
     );
   }
 
-  getSongById(id: number) {
+  getSongById(id: number): any {
     return this.songService.getSongById(id).subscribe(song => {
       this.song = song;
     });
@@ -110,7 +123,21 @@ export class EditSongComponent implements OnInit {
 
   editSong(): any {
     if (confirm('Bạn có chắc chắn muốn sửa thông tin bài hát')) {
-      return this.songService.editSong(this.song.id, this.song).subscribe(window.location.reload);
+      return this.songService.editSong(this.song.id, this.song).subscribe(
+        window.location.reload
+      );
     }
+  }
+
+  getAllCate(): any{
+    this.cateService.getAllCate().subscribe( categories => {
+      this.categories = categories;
+    });
+  }
+  getAllPlayList(): any {
+    this.playlistService.getAllPlayList(this.authen.currentUserValue.username).subscribe(playlist => {
+      this.playList = playlist;
+      console.log(this.playList);
+    }, error => console.log(error));
   }
 }
